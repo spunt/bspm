982fb73c9bbe18202843d90c068231a682a00dfc
Bob Spunt <bobspunt@gmail.com>
2015-11-12 22:17:45 -0800

    misc updates

--- prepoc/bspm_convert_dcm.m
+++ prepoc/bspm_convert_dcm.m
@@ -30,57 +30,68 @@ for sub = 1:nsubs
     subDIR          = subdirs{sub};
     [p, subnam, e]  = fileparts(subDIR);
     fprintf('\n| Working on: %s', subnam);
-    alldcm          = files(fullfile(subDIR, '**', '*dcm'));
-    if isempty(alldcm), fprintf('\n!!! NO .DCM FILES FOR THIS SUBJECT, MOVING ON...'); continue; end
-    [dothese, ia, ic] = unique(cellfun(@fileparts, alldcm, 'unif', false));
+    dothese         = files(fullfile(subDIR, dcmdirpat, '*')); 
+    
+    alldcm          = cellfun(@files, fullfile(dothese, '*dcm'), 'Unif', false); 
+    if isempty(vertcat(alldcm{:})), fprintf('\n!!! NO .DCM FILES FOR THIS SUBJECT, MOVING ON...'); continue; end
+    ndcm            = cellfun('length', alldcm);
+    if any(ndcm==0)
+        fprintf('\n!!! One or more dicom subfolder is empty and is ignored.\n!!! You might want to see what''s up with that.\n');
+        dothese(ndcm==0) = []; 
+        alldcm(ndcm==0) = []; 
+    end
     fprintf('\n| Running DICM2NII on %d dicom folders\n', length(dothese));
-    disp(dothese);
     rawDIR = fullfile(subDIR, 'raw'); mkdir(rawDIR); 
     fprintf('| Output directory: %s\n', rawDIR);
-    
-    seqid = cell(length(dothese), 2);
-    
+    [~,fname] = cellfun(@fileparts, dothese, 'unif', false);
     for s = 1:length(dothese)
+        dcminfo{s} = bspm_get_dicom_info(alldcm{s}(ceil(ndcm(s)/2)), 0);
+        seqinfo(s) = dcminfo{s}.sequenceinfo;
+    end
+    orderinfo = vertcat(seqinfo.order);
+    uid = unique(orderinfo);
+    
+    for s = 1:length(uid)
 
-        dicomfiles  = alldcm(ic==s); 
-        dcmref      = dicomfiles{ceil(length(dicomfiles)/2)}; 
-        dcminfo     = bspm_get_dicom_info(dcmref, 0); 
-        outputDIR   = fullfile(rawDIR, sprintf('%s_%s_%02d', dcminfo.sequenceinfo.type(1:2), dcminfo.sequenceinfo.name, dcminfo.sequenceinfo.order));
+        didx        = find(orderinfo==uid(s)); 
+        dicomfiles  = vertcat(alldcm{didx});
+        dcmref      = vertcat(dcminfo{didx}); 
+        outputDIR   = fullfile(rawDIR, sprintf('%s_%s_%02d', dcmref(1).sequenceinfo.type(1:2), dcmref(1).sequenceinfo.name, uid(s)));
         mkdir(outputDIR);
-        dicm2nii(dothese{s}, outputDIR, 4);
+        dicm2nii(dicomfiles, outputDIR, 4);
         try
-            dcminfo.dcmHeaders = load(fullfile(outputDIR, 'dcmHeaders.mat'));
+            dcmref(1).dcmHeaders = load(fullfile(outputDIR, 'dcmHeaders.mat'));
             delete(fullfile(outputDIR, 'dcmHeaders.mat'));
         catch
         end
-        tmp = dcminfo.sequenceinfo; 
-        seqid{s,1} = sprintf('%s%s%s%d', tmp.name, tmp.type, tmp.pulsename, tmp.timestamp);
-        seqid{s,2} = outputDIR; 
-        save(fullfile(outputDIR, 'dicominfo.mat'), 'dcminfo');
-        nii         = files(fullfile(outputDIR, '*nii'));
+        save(fullfile(outputDIR, 'dicominfo.mat'), 'dcmref');
+%         tmp = dcmref.sequenceinfo; 
         
-        if strcmp(tmp.type, 'EP')
-           if omitfirstN, delete(nii{1:omitfirstN}); nii(1:omitfirstN) = []; end
-           if ~isempty(funcprefix)
-                [pth, fn, fe] = cellfileparts(nii);
-                outfn = strcat(pth, filesep, sprintf('%s_%03d_', funcprefix, dcminfo.sequenceinfo.order), lower(fn), fe);
-                cellfun(@movefile, nii, outfn)
-           end
-           if do3dto4d, bnii_3dto4d(nii, 'compress', 1, 'delete3d', 1, 'delimiter', '_'); end
-        elseif all(strncmp(tmp.type, 'GR', 2), ~isempty(anatprefix))
-            [pth, fn, fe] = cellfileparts(nii);
-            
-            outfn = strcat(pth, filesep, sprintf('%s_%03d_', anatprefix, dcminfo.sequenceinfo.order), lower(fn), fe);
-            cellfun(@movefile, nii, outfn)
-        end
-        
-        if s~=1
-            if all([strcmp(tmp.type, 'GR') strcmp(seqid{s,1}, seqid{s-1,1})])
-                nii         = files(fullfile(outputDIR, '*nii'));
-                movefile(char(nii), seqid{s-1,2});
-                rmdir(outputDIR, 's');
-            end
-        end
+%         seqid{s,1} = sprintf('%s%s%s%d', tmp.name, tmp.type, tmp.pulsename, tmp.timestamp);
+%         seqid{s,2} = outputDIR; 
+%         save(fullfile(outputDIR, 'dicominfo.mat'), 'dcminfo');
+%         nii         = files(fullfile(outputDIR, '*nii'));
+%         
+%         if strcmp(tmp.type, 'EP')
+%            if omitfirstN, delete(nii{1:omitfirstN}); nii(1:omitfirstN) = []; end
+%            if ~isempty(funcprefix)
+%                 [pth, fn, fe] = cellfileparts(nii);
+%                 outfn = strcat(pth, filesep, sprintf('%s_%03d_', funcprefix, dcminfo.sequenceinfo.order), lower(fn), fe);
+%                 cellfun(@movefile, nii, outfn)
+%            end
+%            if do3dto4d, bnii_3dto4d(nii, 'compress', 1, 'delete3d', 1, 'delimiter', '_'); end
+%         elseif all(strncmp(tmp.type, 'GR', 2), ~isempty(anatprefix))
+%             [pth, fn, fe] = cellfileparts(nii);
+%             outfn = strcat(pth, filesep, sprintf('%s_%03d_', anatprefix, dcminfo.sequenceinfo.order), lower(fn), fe);
+%             cellfun(@movefile, nii, outfn)
+%         end
+%         if s~=1
+%             if all([strcmp(tmp.type, 'GR') strcmp(seqid{s,1}, seqid{s-1,1})])
+%                 nii         = files(fullfile(outputDIR, '*nii'));
+%                 movefile(char(nii), seqid{s-1,2});
+%                 rmdir(outputDIR, 's');
+%             end
+%         end
         
     end
     
