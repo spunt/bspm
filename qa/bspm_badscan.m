1028db968a7f59b00226ce6557de0264fe05b17e
Bob Spunt <bobspunt@gmail.com>
2015-11-17 18:09:00 -0800

    misc updates

--- qa/bspm_badscan.m
+++ qa/bspm_badscan.m
@@ -1,17 +1,17 @@
 function tsinfo = bspm_badscan(epi, varargin)
 % BSPM_BADSCAN Identifies bad scans and saves a nuisance regressor file
-% 
+%
 %   USAGE: tsinfo = bspm_badscan(epi, varargin)
 %
 %  ARGUMENTS
 %    epi = cell array of EPI filenames
-%   'dvars_thresh',     2.5,  
-%   'framewise_thresh', 0.5,  
+%   'dvars_thresh',     2.5,
+%   'framewise_thresh', 0.5,
 %   'include_rp',       1,
-%   'prefix',           'badscan', 
+%   'prefix',           'badscan',
 %   'maskfile',         []
-%   
-% 
+%
+%
 % Bob Spunt, Caltech
 % Based on a script by Donald McLaren, Ph.D.
 % 2012_05_01 -- Created
@@ -26,19 +26,24 @@ def = { 'dvars_thresh',     2.5,  ...
 vals = setargs(def, varargin);
 if nargin==0, mfile_showhelp; fprintf('\t| - VARARGIN DEFAULTS - |\n'); disp(vals); return; end
 
+% | PATH TO BRAMILA TOOLS
+bramiladir = fullfile(getenv('HOME'), 'Github', 'bspm', 'thirdparty', 'bramila');
+addpath(bramiladir)
+
+
 % | get data and apply implicit masking
 if ischar(epi), epi = cellstr(epi); end
-fprintf('\n | - Loading Data'); 
-cfg.plot            = 0; 
+fprintf('\n | - Loading Data');
+cfg.plot            = 0;
 if isempty(maskfile)
     cfg.vol     = bspm_read_vol(epi, 'implicit');
 else
-    cfg.vol     = bspm_read_vol(epi, 'mask', maskfile); 
+    cfg.vol     = bspm_read_vol(epi, 'mask', maskfile);
 end
 
 % | create output filename
 outfile     = sprintf('%s_dvars%dframewise%d_%s.txt', prefix, dvars_thresh*100, framewise_thresh*100, strtrim(datestr(now,'mmm_DD_YYYY')));
-nvol        = size(cfg.vol, 4); 
+nvol        = size(cfg.vol, 4);
 epidir      = fileparts(epi{1});
 
 % | motion parameters
@@ -54,15 +59,15 @@ dvars       = bramila_dvars(cfg);
 % | get indices of bad timepoints
 zdvars          = oneoutzscore(dvars(2:end), 1);
 badidx          = zeros(nvol, 2);
-badidx(2:end,1) = zdvars > dvars_thresh; 
+badidx(2:end,1) = zdvars > dvars_thresh;
 badidx(:,2)     = framewise > framewise_thresh;
 badidx          = find(any(badidx, 2));
-nbad            = length(badidx); 
+nbad            = length(badidx);
 tsinfo.pctbad   = round(100*(nbad/nvol));
 fprintf('\n | - %d (%d%%) scans identified as bad', nbad, tsinfo.pctbad);
 
 % | create bad volume regressor
-scrubmat = zeros(nvol, nbad); 
+scrubmat = zeros(nvol, nbad);
 for i = 1:length(badidx), scrubmat(badidx(i),i) = 1; end
 if include_rp==1, scrubmat = [rp scrubmat]; end
 
@@ -71,12 +76,14 @@ save(fullfile(epidir, outfile), 'scrubmat', '-ascii');
 fprintf('\n | - Nuisance regressors written to:  %s\n\n', outfile);
 
 if nargout>0
-    tsinfo.epidir       = epidir; 
+    tsinfo.epidir       = epidir;
     tsinfo.maxmotion    = max(rp) - min(rp);
-    tsinfo.dvars        = dvars; 
+    tsinfo.dvars        = dvars;
     tsinfo.framewise    = framewise;
-    tsinfo.scrubmat     = scrubmat; 
+    tsinfo.scrubmat     = scrubmat;
 end
+
+rmpath(bramiladir)
 
 end
 %---------------------------------------------------------------------%
@@ -84,15 +91,15 @@ end
 %---------------------------------------------------------------------%
 function zx = oneoutzscore(x, returnas)
 % ONEOUTZSCORE Perform columnwise leave-one-out zscoring
-% 
+%
 % USAGE: zx = oneoutzscore(x, returnas)
-% 
+%
 %   returnas: 0, signed values (default); 1, absolute values
 %
 if nargin<1, disp('USAGE: zx = oneoutzscore(x, returnas)'); return; end
 if nargin<2, returnas = 0; end
 if size(x,1)==1, x=x'; end
-zx              = x; 
+zx              = x;
 [nrow, ncol]    = size(x);
 for c = 1:ncol
     cin         = repmat(x(:,c), 1, nrow);
@@ -103,7 +110,7 @@ for c = 1:ncol
 end
 if returnas, zx = abs(zx); end
 end
- 
- 
- 
- 
+
+
+
+
