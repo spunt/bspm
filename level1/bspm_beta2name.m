659c704f9898da1eb9cf4aeae9ad1adba5756c88
Bob Spunt <bobspunt@gmail.com>
2016-01-19 10:44:49 -0800

    misc updates

---
+++ level1/bspm_beta2name.m
@@ -0,0 +1,11 @@
+function name = bspm_beta2name(beta)
+% BSPM_BETA2NAME
+% USAGE: name = bspm_beta2name(beta)
+%
+if nargin<1, mfile_showhelp; return; end
+if iscell(beta), beta = char(beta); end
+h       = spm_vol(beta);
+name    = {h.descrip}'; 
+name    = regexprep(name, '^.*Sn\(\d\)\s', ''); 
+name    = regexprep(name, '\*bf\(\d\)', '');
+end
